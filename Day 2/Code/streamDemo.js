61e14c228a0042950d95bf94860453b78d4e6379
Siddhesh Prabhugaonkar <siddheshpg@gmail.com>
2025-11-07 14:41:09 +0530

    aded assignments

--- Day 2/Code/streamDemo.js
+++ Day 2/Code/streamDemo.js
@@ -2,10 +2,10 @@ import fs from 'fs';
 import { Transform } from 'stream';
 
 fs.createReadStream("input.txt", "utf-8")
-    .pipe(toUpperCase())
+    .pipe(upperCase())
     .pipe(fs.createWriteStream("output.txt"));
 
-function toUpperCase() {
+function upperCase() {
     return new Transform({
         transform(chunk, encoding, callback) {
             const upperChunk = chunk.toString().toUpperCase();
