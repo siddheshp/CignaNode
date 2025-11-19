866af7d64ea20bc03cda099a810c9ab7fc87c95e
Siddhesh Prabhugaonkar <siddheshpg@gmail.com>
2025-11-19 10:33:22 +0530

    uni test content

--- Day 9/ems-api-jest/src/employees/employees.service.spec.ts
+++ Day 9/ems-api-jest/src/employees/employees.service.spec.ts
@@ -29,7 +29,7 @@ describe('EmployeesService', () => {
     service = module.get<EmployeesService>(EmployeesService);
   });
 
-  afterEach(()=>{
+  afterEach(() => {
     jest.clearAllMocks();
   });
 
@@ -37,5 +37,40 @@ describe('EmployeesService', () => {
     expect(service).toBeDefined();
   });
 
-  const mockEmployees: Employee[] = [ { id: 1, name: 'Ravi', email: 'ravi@test.com', salary: 50000, dateOfBirth: new Date('1990-01-01'), mobileNumber: 1234567890, departmentId: 1 }, { id: 2, name: 'Priya', email: 'priya@test.com', salary: 60000, dateOfBirth: new Date('1992-02-02'), mobileNumber: 9876543210, departmentId: 2 }, ];
+  it('find all employees', async () => {
+    //Arrange
+    const mockEmployees: Employee[] = [
+      { id: 1, name: 'Ravi', email: 'ravi@test.com', salary: 50000, dateOfBirth: new Date('1990-01-01'), mobileNumber: 1234567890, departmentId: 1 },
+      { id: 2, name: 'Priya', email: 'priya@test.com', salary: 60000, dateOfBirth: new Date('1992-02-02'), mobileNumber: 9876543210, departmentId: 2 },
+    ];
+
+    mockRepo.find?.mockResolvedValue(mockEmployees);
+
+    //Act
+    const actual = await service.findAll();
+    //assert
+    expect(actual).toEqual(mockEmployees);
+    expect(actual).toHaveLength(2);
+    expect(mockRepo.find).toHaveBeenCalled();
+  })
+
+  it('should return one employee by id', async () => {
+    //Arrange
+    const mockEmployee: Employee = {
+      id: 1, name: 'Ravi', email: 'ravi@test.com', salary: 50000,
+      dateOfBirth: new Date('1990-01-01'), mobileNumber: 1234567890, departmentId: 1,
+    };
+
+    mockRepo.findOneBy?.mockResolvedValue(mockEmployee);
+
+    //Act
+    const actual = await service.findOne(1);
+
+    //Assert
+    expect(actual).toEqual(mockEmployee);
+    // expect(actual.email).toEqual(mockEmployee.email);
+    expect(mockRepo.findOneBy).toHaveBeenCalledWith({id: 1});
+  })
+
+
 });
